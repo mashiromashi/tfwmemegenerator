f9c1a194096baa6d2a992d738cd4af9d0e803984
Htin Wana <dotodatadete@gmail.com>
2019-05-08 14:21:39 +0630

    added Burmese templates

---
+++ server/db/model/templateModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const templateModel = new Schema({
+  id: { type: integer },
+  name: { type: string },
+  url: { type: string },
+  box_count: { type: integer },
+  width: { type: integer },
+  height: { type: integer }
+});
+
+export default mongoose.model("template", templateModel);
